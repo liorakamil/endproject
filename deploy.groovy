b3937199b9dbbaa71692e830c30128b42bb99029
Liora Kamil <liorakamil@Assafs-MacBook-Pro.local>
2020-04-09 17:38:33 +0300

    deploy

---
+++ deploy.groovy
@@ -0,0 +1,17 @@
+node("linux") {
+ def customImage = ""
+ stage("pull code") {
+     checkout scm
+ }
+
+  stage("build docker") {
+    customImage = docker.build("liorakamil/endproject:${env.BUILD_ID}")
+    withDockerRegistry(credentialsId: 'dockerhub') {
+        customImage.push()
+    }
+ }
+
+ stage("verify dockers") {
+  sh "docker images"
+ }
+}
