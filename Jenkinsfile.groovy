fb544e1eee25824f85e3cea27c09191152df741e
Liora Kamil <liorakamil@Assafs-MacBook-Pro.local>
2020-04-09 23:02:14 +0300

    k8s

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,14 +4,60 @@ node("linux") {
      checkout scm
  }
 
-  stage("build docker") {
-    customImage = docker.build("liorakamil/endproject:${env.BUILD_ID}")
-    withDockerRegistry(credentialsId: 'dockerhub') {
-        customImage.push()
+ stage("build docker") {
+     customImage = docker.build("liorakamil/endproject:${env.BUILD_ID}")
+     withDockerRegistry(credentialsId: 'dockerhub') {
+         customImage.push()
     }
  }
 
  stage("verify dockers") {
-  sh "docker images"
+     sh "docker images"
  }
+}
+
+stage('Apply Kubernetes files') {
+    withAWS(region: 'us-east-1', credentials: 'AWSK8S') {
+sh """
+aws eks update-kubeconfig --name eks-cluster-flask
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1      # for versions before 1.9.0 use apps/v1beta2
+kind: Service
+metadata:
+  name: flask-service
+  labels:
+    app: flask
+spec:
+  type: LoadBalancer
+  ports:
+  - protocol: TCP
+    port: 80
+    targetPort: 5000
+  selector:
+    app: flask
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: flask-deployment
+spec:
+  selector:
+    matchLabels:
+      app: flask
+  replicas: 2 # tells deployment to run 2 pods matching the template
+  template:
+    metadata:
+      labels:
+        app: flask
+    spec:
+      containers:
+      - name: flask
+        image: liorakamil/endproject:${env.BUILD_ID}
+        ports:
+        - containerPort: 5000
+EOF
+"""
+    }
+  }
 }
